bef6f7075f24e3ca20280bc0361f82d43e91d7f3
smsqadri <smsqadri@gmail.com>
2020-06-24 05:13:33 +0300

    update color

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
 
   return (
     <div className={`box ${isMorning ? 'dayLight' : ''}`}>
-      <h1>Day Time = {isMorning ? 'Morning' : 'Night'}</h1>
+      <h1>Good = {isMorning ? 'Morning' : 'Night'}</h1>
       <Message counter={count}/>
       <br/>
       <button onClick={()=> setCount(count + 1)}>Update Counter Method 1 with count + 1</button>
       <br/> <br/>
       <button onClick={()=> setCount(++count)}>Update Counter Method 2 with ++count</button>
       <br/> <br/>
-      <button onClick={() => setMorning(!isMorning)}>Day / Night</button>
+      <button onClick={() => setMorning(!isMorning)}>Din / Raat</button>
     </div>
   );
 }
